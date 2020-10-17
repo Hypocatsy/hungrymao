a86c392e9be824cd850981a0e04abe7236c92070
Hypocatsy <gbearxx@gmail.com>
2020-10-17 22:10:16 +0800

    randomise function

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ var degree = 1800;
 //number of clicks = 0
 var clicks = 0;
 
+// Part 1: Wheel Info - cuisines and relevant icons
+var choiceArray = [
+    {"cuisine" : "Chinese", "icon": '<img src="media/wheel_icons/chinese.png" height="50" width="50">' }, 
+    {"cuisine" : "Korean", "icon": '<img src="media/wheel_icons/korean.png" height="50" width="50">' }, 
+    {"cuisine" : "Japanese", "icon": '<img src="media/wheel_icons/japanese.png" height="50" width="50">' }, 
+    {"cuisine" : "Italian", "icon": '<img src="media/wheel_icons/italian.png" height="50" width="50">' }, 
+    {"cuisine" : "Indian", "icon": '<img src="media/wheel_icons/indian.png" height="50" width="50">' }, 
+    {"cuisine" : "Mexican", "icon": '<img src="media/wheel_icons/mexican.png" height="50" width="50">' }, 
+];
 
 // put array info into wheel
-function defaultview() {
+function defaultview(choiceArray) {
 
-	// Part 1: Wheel Info - cuisines and relevant icons
-	var choiceArray = [
-		{"cuisine" : "Chinese", "number": Math.random(0, 5),  "icon": '<img src="../is216-Project/media/wheel_icons/chinese.png" height="50" width="50">' }, 
-		{"cuisine" : "Korean", "number": Math.random(0, 5),  "icon": '<img src="../is216-Project/media/wheel_icons/korean.png" height="50" width="50">' }, 
-		{"cuisine" : "Japanese", "number": Math.random(0, 5), "icon": '<img src="../is216-Project/media/wheel_icons/japanese.png" height="50" width="50">' }, 
-		{"cuisine" : "Italian", "number": Math.random(0, 5), "icon": '<img src="../is216-Project/media/wheel_icons/italian.png" height="50" width="50">' }, 
-		{"cuisine" : "Indian", "number": Math.random(0, 5),  "icon": '<img src="../is216-Project/media/wheel_icons/indian.png" height="50" width="50">' }, 
-		{"cuisine" : "Mexican", "number": Math.random(0, 5),  "icon": '<img src="../is216-Project/media/wheel_icons/mexican.png" height="50" width="50">' }, 
-	];
 
 	var wheelstr = '';
 	for (choiceObject of choiceArray) {
@@ -84,7 +84,7 @@ $(document).ready(function(){
 
 				var aoY = t.offset().top;
 				$("#txt").html(aoY);
-				console.log(aoY);
+				// console.log(aoY);
 
 				/*23.7 is the minumum offset number that
 				each section can get, in a 30 angle degree.
@@ -96,7 +96,7 @@ $(document).ready(function(){
 					$('#spin').addClass('spin');
 					setTimeout(function () {
 						$('#spin').removeClass('spin');
-					}, 100);
+					}, 80);
 
 				}
 			}, 10);
@@ -120,18 +120,26 @@ $(document).ready(function(){
 
 });//DOCUMENT READY
 
+function randomize(choiceArray){
+    var i = Math.floor(Math.random() * 5);
+    return choiceArray[i];
+}
 
-function chosen_view(chosen_cuisine="Korean") {
+function chosen_view() {
 	console.log("=== CHOSEN_VIEW() starts ===");
+
+    var chosen_cuisine = randomize(choiceArray);
+    // alert('called randomise')
+    console.log(chosen_cuisine);
 
 	// insert text
 	var msg = `
-	<i class='fas fa-utensils' style='font-size:48px;' id='fork_knife'></i>
+	<span class="fa text">${chosen_cuisine.icon}</span>
 	<div id="beside_wheel_text">
-		<h4>Yay!</h4> 
-		You are fated to eat ${chosen_cuisine}
+		<h4>Yay😋</h4> 
+		You are fated to eat ${chosen_cuisine.cuisine} food!
 	</div>
-	<button type="button" class="btn" id="no_button">No! I want to spin again!!</button>
+	<button type="button" class="btn" id="no_button">Confirm cuisine and proceed</button>
 	`;
 	document.getElementById("wheeltextdiv").innerHTML = msg;
 
